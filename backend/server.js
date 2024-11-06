e7dfebadc80224cf7a1b37c0ca175c8021edc26b
Christopher Vinantius <christopher.vinantius@binus.ac.id>
2024-11-07 01:42:04 +0700

    adjust for deployment

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.use(cors({ origin: "http://localhost:5173" }))
+app.use(cors())
+
+app.use(cors({
+    origin: 'https://compsci-helper.vercel.app/'
+}))
 
 app.use(express.json())
 
