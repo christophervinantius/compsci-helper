3496b76cb58fed2d656b0c4d4335bdd947bcf866
Christopher Vinantius <christopher.vinantius@binus.ac.id>
2024-11-10 00:31:40 +0700

    test adjust

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,14 @@
 import express from 'express'
-import cors from 'cors'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js'
 import postRoutes from './routes/post.route.js'
 import path from 'path'
 
-dotenv.config().parsed
+dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
 const __dirname = path.resolve()
-
-const allowedOrigins = process.env.NODE_ENV === 'production' 
-    ? ['https://compsci-helper.vercel.app']
-    : ['https://compsci-helper.vercel.app', 'http://localhost:5173']
-
-app.use(cors({
-    origin: allowedOrigins
-}))
 
 app.use(express.json())
 app.use('/api/posts', postRoutes)
