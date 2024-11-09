b863ca77f51a02b6d4e25cc9dcd8883b7d5f4183
Christopher Vinantius <christopher.vinantius@binus.ac.id>
2024-11-09 23:54:02 +0700

    test deploy on render

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,13 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js'
 import postRoutes from './routes/post.route.js'
+import path from 'path'
 
 dotenv.config().parsed
 
 const app = express()
-
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
 
 const allowedOrigins = process.env.NODE_ENV === 'production' 
     ? ['https://compsci-helper.vercel.app']
@@ -21,8 +22,15 @@ app.use(cors({
 app.use(express.json())
 app.use('/api/posts', postRoutes)
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+}
+
 app.listen(PORT, () => {
     connectDB()
     console.log('Server started on http://localhost:' + PORT)
-    console.log('Running in ' + process.env.NODE_ENV + ' mode')
 })
